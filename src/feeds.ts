5abd9f8fe22173db366ffda77de737bcceb85b1b
Saturday-morning <53111574+Saturday-morning@users.noreply.github.com>
2023-04-11 23:33:39 +0800

    Add files via upload

--- src/feeds.ts
+++ src/feeds.ts
@@ -1,10 +1,24 @@
 export const feeds = [
-  // Daring Fireball
-  'https://daringfireball.net/feeds/main',
+  // 测试用例
+  //'https://theoldreader.com/profile/tamakiui.rss',
 
-  // JakeArchibald.com
-  'https://jakearchibald.com/posts.rss',
+  //知乎
+  'https://atom.mgrc.workers.dev/zhihu/people/activities/yumi_youmei',
 
-  // Appjeniksaan
-  'https://appjeniksaan.nl/feed.xml',
+  //bilibili
+  //#魔法纪录#
+  'https://atom.mgrc.workers.dev/bilibili/user/video/679627',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/589391011',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/76552180',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/29959853',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/13767834',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/246606859',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/3493090790738007',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/43656424',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/381973210',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/8574068',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/636862680',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/12636356',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/13767834',
+  'https://atom.mgrc.workers.dev/bilibili/user/video/194242416',
 ]
